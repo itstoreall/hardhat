cb615e0d5b66f4fe5f88670bf49d37c2ef70b76e
Serhii Stanislav <serhiistanislav@gmail.com>
2023-04-26 14:30:50 +0300

    ADD | lottery

---
+++ notes/lottery.js
@@ -0,0 +1,37 @@
+/*
+
+yarn commands:
+yarn add --dev hardhat
+yarn hardhat
+yarn hardhat compile
+
+mkdir lottery
+yarn add --dev hardhat
+yarn hardhat
+
+for js:
+yarn add --dev @nomiclabs/hardhat-ethers@npm:hardhat-deploy-ethers ethers @nomiclabs/hardhat-etherscan @nomiclabs/hardhat-waffle chai ethereum-waffle hardhat hardhat-contract-sizer hardhat-deploy hardhat-gas-reporter prettier prettier-plugin-solidity solhint solidity-coverage dotenv 
+
+for ts:
+yarn add --dev @nomiclabs/hardhat-ethers@npm:hardhat-deploy-ethers ethers @nomiclabs/hardhat-etherscan @nomiclabs/hardhat-waffle chai ethereum-waffle hardhat hardhat-contract-sizer hardhat-deploy hardhat-gas-reporter prettier prettier-plugin-solidity solhint solidity-coverage dotenv @typechain/ethers-v5 @typechain/hardhat @types/chai @types/node ts-node typechain typescript 
+
+.prettierrc:
+{
+  "tabWidth": 4,
+  "useTabs": false,
+  "semi": false,
+  "singleQuote": false,
+  "printWidth": 100
+}
+
+hardhat.config.js:
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+
+*/
